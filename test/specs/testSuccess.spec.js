0d22313ad39774611a8605b0d448211654fdb2fa
jeff.flater <jeff.flater@semaphoredoes.com>
2014-07-23 12:09:18 -0400

    fleshed out success specs

--- test/specs/testSuccess.spec.js
+++ test/specs/testSuccess.spec.js
@@ -29,13 +29,63 @@ describe("Test Mock Success Scenarios", function() {
     });
 
     it("Test successful response for mock api/foo NOT using queryString parameters", function(done){
-        expect(true).toEqual(true);
-        done();
+
+        var testTemplate1 = function(){ return '{ "name": "{{firstName}}", "age": {{number 18 65}} }' };
+        var testTemplate2 = function(){ return '{ "name": "{{firstName}}", "age": {{number 18 65}} }' };
+        var testTemplate3 = function(){ return '{ "name": "{{firstName}}", "age": {{number 18 65}} }' };
+
+        var routes = mock({
+            jsonStore: 'test/data/data.json',
+            mockRoutes: [
+                {
+                    name: routeName,
+                    mockRoute: routePath,
+                    testScope: testScope,
+                    testScenario: 1,
+                    jsonTemplate: [testTemplate1, testTemplate2, testTemplate3]
+                }
+            ]
+        });
+
+        app.use(routes.registerRoutes);
+
+        var url = baseUrl+':'+port+routePath;
+
+        request(url, function(error, response, body){
+            expect(validator.isJSON(body)).toEqual(true);
+            expect(response.statusCode).toEqual(statusCode);
+            done();
+        });
+
     });
 
     it("Test successful response for mock api/foo using scope/scenario queryString parameters", function(done){
-        expect(true).toEqual(true);
-        done();
+
+        var testTemplate1 = function(){ return '{ "name": "{{firstName}}", "age": {{number 18 65}} }' };
+        var testTemplate2 = function(){ return '{ "name": "{{firstName}}", "age": {{number 18 65}} }' };
+        var testTemplate3 = function(){ return '{ "name": "{{firstName}}", "age": {{number 18 65}} }' };
+
+        var routes = mock({
+            jsonStore: 'test/data/data.json',
+            mockRoutes: [
+                {
+                    name: routeName,
+                    mockRoute: routePath,
+                    jsonTemplate: [testTemplate1, testTemplate2, testTemplate3]
+                }
+            ]
+        });
+
+        app.use(routes.registerRoutes);
+
+        var url = baseUrl+':'+port+routePath+'?scope='+testScope+'&scenario='+testScenario;
+
+        request(url, function(error, response, body){
+            expect(validator.isJSON(body)).toEqual(true);
+            expect(response.statusCode).toEqual(statusCode);
+            done();
+        });
+
     });
 
 });
